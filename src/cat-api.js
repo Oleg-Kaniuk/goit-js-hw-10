97116da4e3727e11683608f20f786bfb1abf32c5
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-08-27 22:05:35 +0300

    add fix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,27 +1,29 @@
-const base_url = "https://api.thecatapi.com/v1";
-const end_point = "/breeds"
-const end_point_two = "/images/search"
-const api_key = "live_3vZ932cA3h4VazEOwxEEC0YV5LIOFsUTwToRXNlwal9kDv6iW9m3OXoEc1LpZPrR";
+// Оголошення базового URL, кінцевих точок та ключа API
+const base_url = "https://api.thecatapi.com/v1"; // Базовий URL API
+const end_point = "/breeds"; // Кінцева точка для отримання інформації про породи
+const end_point_two = "/images/search"; // Кінцева точка для отримання зображень котів
+const api_key = "live_3vZ932cA3h4VazEOwxEEC0YV5LIOFsUTwToRXNlwal9kDv6iW9m3OXoEc1LpZPrR"; // Ключ API
 
-
+// Функція для отримання інформації про породи котів
 export function fetchBreeds() {
     return fetch(`${base_url}${end_point}?api_key=${api_key}`)
         .then(response => {
-            console.log(response);
-           if (!response.ok) {
-                throw new Error(response.statusText);
+            console.log(response); // Виведення об'єкту відповіді в консоль для дебагу
+            if (!response.ok) {
+                throw new Error(response.statusText); // Обробка помилок відповіді
             }
-            return response.json();
+            return response.json(); // Парсинг відповіді у форматі JSON
         });
 };
 
+// Функція для отримання зображення кота за певною породою
 export function fetchCatByBreed(breedId) {
     return fetch(`${base_url}${end_point_two}?api_key=${api_key}&breed_ids=${breedId}`)
         .then(response => {
-        console.log(response);
+            console.log(response); // Виведення об'єкту відповіді в консоль для дебагу
             if (!response.ok) {
-                throw new Error(response.statusText);
+                throw new Error(response.statusText); // Обробка помилок відповіді
             }
-            return response.json();
+            return response.json(); // Парсинг відповіді у форматі JSON
         });
 };
--- src/index.js
+++ src/index.js
@@ -62,50 +62,71 @@
 //     .catch(onErrorFetch);
 // }
 
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
-import './styles.css';
+// Імпорт необхідних модулів та бібліотек
+import { fetchBreeds, fetchCatByBreed } from "./cat-api"; // Імпорт функцій з cat-api.js
+import Notiflix from 'notiflix'; // Імпорт бібліотеки для сповіщень
+import SlimSelect from 'slim-select'; // Імпорт бібліотеки для стилізованого селекта
+import 'slim-select/dist/slimselect.css'; // Імпорт стилів для стилізованого селекта
+import './styles.css'; // Імпорт власних стилів з styles.css
+
+// Оголошення елементів сторінки
 const elements = {
-    select: document.querySelector('.breed-select'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-    catInfo: document.querySelector('.cat-info'),
+    select: document.querySelector('.breed-select'), // Вибір породи (селект)
+    loader: document.querySelector('.loader'), // Завантажувач
+    error: document.querySelector('.error'), // Повідомлення про помилку
+    catInfo: document.querySelector('.cat-info'), // Інформація про кота
 };
+
+// Розпакування елементів сторінки
 const { select, loader, error, catInfo } = elements;
+
+// Приховання селекту та відображення завантажувача
 select.classList.add('is-hidden');
 error.classList.add('is-hidden');
 loader.classList.remove('is-hidden');
+
+// Масив для опцій вибору породи кота
 let breedOptions = [];
+
+// Отримання інформації про породи котів і ініціалізація селекта
 fetchBreeds()
     .then(data => {
         breedOptions = data.map(breed => ({ value: breed.id, text: breed.name }));
         initializeSelect();
         select.classList.remove('is-hidden'); // Відобразити селект
-        loader.classList.add('is-hidden');
+        loader.classList.add('is-hidden'); // Приховати завантажувач
     })
-    .catch(onErrorFetch);
+    .catch(onErrorFetch); // Обробка помилок при отриманні даних
+
+// Ініціалізація стилізованого селекта
 function initializeSelect() {
     new SlimSelect({
         select: select,
         data: breedOptions
     });
-    select.addEventListener('change', onSelectBreedCats);
+    select.addEventListener('change', onSelectBreedCats); // Додати обробник події для вибору породи
 }
+
+// Обробка помилок при отриманні даних
 function onErrorFetch() {
-    loader.classList.add('is-hidden'); // Приховати лоадер
+    loader.classList.add('is-hidden'); // Приховати завантажувач
     select.classList.remove('is-hidden'); // Відобразити селект
-    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!'); // Повідомлення про помилку
 }
+
+// Обробник події вибору породи кота
 function onSelectBreedCats(evt) {
-    loader.classList.remove('is-hidden');
-    select.classList.add('is-hidden');
-    catInfo.classList.add('is-hidden');
+    loader.classList.remove('is-hidden'); // Відобразити завантажувач
+    select.classList.add('is-hidden'); // Приховати селект
+    catInfo.classList.add('is-hidden'); // Приховати інформацію про кота
+
+    // Отримання інформації про кота обраної породи
     const breedId = evt.target.value;
     fetchCatByBreed(breedId)
         .then(data => {
             const { url, breeds } = data[0];
+
+            // Відображення інформації про кота
             catInfo.innerHTML = `
                 <div class="img">
                     <img src="${url}" alt="${breeds[0].name}" width="600"/>
@@ -116,11 +137,11 @@ function onSelectBreedCats(evt) {
                     <p><i><b>Life_span: </b></i>${breeds[0].life_span} years</p>
                     <p><i><b>Temperament:</b></i> ${breeds[0].temperament}</p>
                 </div>`;
-            catInfo.classList.remove('is-hidden');
+            catInfo.classList.remove('is-hidden'); // Відобразити інформацію про кота
         })
-        .catch(onErrorFetch)
+        .catch(onErrorFetch) // Обробка помилок при отриманні даних
         .finally(() => {
-            loader.classList.add('is-hidden');
-            select.classList.remove('is-hidden');
+            loader.classList.add('is-hidden'); // Приховати завантажувач
+            select.classList.remove('is-hidden'); // Відобразити селект
         });
 }
